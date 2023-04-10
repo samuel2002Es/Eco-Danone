64864f999ce1da46c82a037fa2df79918c9c4b9b
samuel <samuel2002espinoza@gmail.com>
2023-04-10 16:03:11 -0600

    avanzando con material desing

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,56 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <v-app>
+    <v-main>
+      <v-toolbar 
+          :color="primary"
+          :dark="true"
+        >
+          <v-app-bar-nav-icon @click="menu"></v-app-bar-nav-icon>
+          <v-toolbar-title>Title</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+          <v-btn icon>
+            <v-icon>mdi-heart</v-icon>
+          </v-btn>
+          <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+      </v-toolbar>
+      <v-navigation-drawer
+        v-model="drawer"
+        temporary
+      >
+        <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/men/78.jpg"
+          title="John Leider"
+        ></v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
+          <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  data: () => ({
+    drawer:false
+  }),
+  methods:{
+    menu(){
+      this.drawer=true
+    }
+  }
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
---
+++ src/views/Grid.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container class="bg-surface-variant">
+    <v-row no-gutters>
+      <v-col
+        v-for="n in 3"
+        :key="n"
+        cols="12"
+        sm="4"
+      >
+        <v-sheet class="ma-2 pa-2">
+          One of three columns
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container >
+  <v-container class="bg-blue" style="display: grid; height: 20rem; width: auto; align-content: center;">
+    <v-sheet class="d-flex flex-wrap flex-row mb-6 justify-space-around align-center" min-height="100">
+      <v-sheet ><v-btn color="primary">Button</v-btn></v-sheet>
+      <v-sheet ><v-btn color="blue" dark>Button</v-btn></v-sheet>
+      <v-sheet ><v-btn color="secondary" flat >Button</v-btn></v-sheet>
+      <v-sheet ><v-btn color="danger" size="large">Button</v-btn></v-sheet>
+      <v-sheet ><v-btn color="danger" size="large">Button</v-btn></v-sheet>
+      <v-sheet ><v-btn prepend-icon="mdi-vuetify">Button</v-btn></v-sheet>
+      <v-sheet > <v-btn
+        class="ma-2"
+        variant="text"
+        icon="mdi-thumb-up"
+        color="blue-lighten-2"
+      ></v-btn></v-sheet>
+      <v-btn
+        class="ma-2"
+        color="indigo"
+        icon="mdi-cloud-upload"
+      ></v-btn>
+      <v-sheet > <v-btn
+        class="ma-2"
+        color="indigo"
+        icon="mdi-cloud-upload"
+      ></v-btn></v-sheet>
+      <v-btn block>Block Button</v-btn>
+    </v-sheet>
+  </v-container>
+</template>
