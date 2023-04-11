9aaff997c89c4730486dbb772587f6c8ac32484e
samuel <samuel2002espinoza@gmail.com>
2023-04-11 16:41:40 -0600

    terminacion de configuracion en el main.js para que funcione firebase

--- src/main.js
+++ src/main.js
@@ -5,6 +5,22 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 
+import { initializeApp } from "firebase/app";
+/* para utilizar bases de datos no relacionales en firebase utilizamos  */
+import 'firebase/firestore';
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDCOOCvTpB7vhYgK0lmRHlnJ6Rw4mUTTqE",
+  authDomain: "vue-calendario-a2788.firebaseapp.com",
+  projectId: "vue-calendario-a2788",
+  storageBucket: "vue-calendario-a2788.appspot.com",
+  messagingSenderId: "789704547873",
+  appId: "1:789704547873:web:7dca6bf65f335a57bf0d60"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 loadFonts()
 
 createApp(App)
--- src/views/Calendario.vue
+++ src/views/Calendario.vue
@@ -1,3 +1,5 @@
+<!-- COMENTS -->
+<!-- installe vue-textarea-autosize  esto con el fin de que cuando haga text areas sean ajustables segun lo que se le introdusca-->
 <template>
     <h1>calendario</h1>
 </template>
